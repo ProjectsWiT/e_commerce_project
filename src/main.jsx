3a4f5969baf767507b7462944299121862f1d2e5
Nuri Yıkılmaz <nuriyikilmaz@icloud.com>
2025-12-24 18:24:40 +0300

    feat: add axios client toastify and lucide

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 import App from "./App.jsx";
 import "./index.css";
 import store from "./store";
@@ -9,6 +12,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
+      <ToastContainer position="top-right" autoClose={2500} />
     </Provider>
   </React.StrictMode>
 );
