ed3292c88478a601b12749674896bef439e3f864
nehiraslan0909 <nehirraslan09@gmail.com>
2026-01-06 23:28:56 +0300

    sa

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="border-t mt-8">
       <div className="max-w-4xl mx-auto px-4 h-16 flex items-center justify-center text-sm text-gray-500">
-        Footer
+        Footerx
       </div>
     </footer>
   );
